17ad863e430cf7809f8210f614fde7f6479a15cf
bagdadik99 <bagdadi.k@husky.neu.edu>
2019-04-23 20:57:28 -0400

    Modified dao to create admin

--- data/dao/user.dao.server.js
+++ data/dao/user.dao.server.js
@@ -19,26 +19,13 @@ findUserByEmail = (useremail) => userModel.findOne({email: useremail});
 populateUsersSchema = () => {
 
     // Populate Users
-    const alice = {
-        _id: 123,
-        firstName: "Alice",
-        lastName: "Wonderland",
-        username: "alice",
-        password: "alice",
-        image: "URL",
-        email: "alice@wonderland.com"
+    const admin = {
+        username: "admin",
+        password: "admin",
+        email: "admin@xyz.com",
+        role: "ADMIN"
     };
-    const bob = {
-        _id: 234,
-        firstName: "Bob",
-        lastName: "Builder",
-        username: "bob",
-        password: "bob",
-        image: "URL",
-        email: "bob@theBuilder.com"
-    };
-    createUser(alice);
-    createUser(bob);
+    createUser(admin);
 
     return { "message": "Populated database successfully"};
 };
